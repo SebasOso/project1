098509e4d847786950678fc415eba28b079272c9
datsumo <criptodesc09@gmail.com>
2022-09-13 22:01:26 -0500

    nuevo commit en pc grande

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 int main()
 {
 	printf("La vida es bella y linda!!!\n");
-	return(EXIT_SUCCESS);
+	
+	printf("La vida es bella si luchas por ella\n");
+	
 	printf("La vida es bella si luchas por ella\n");
 	return(EXIT_SUCCESS);
 }
